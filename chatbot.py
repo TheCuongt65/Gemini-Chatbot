00cc8a2792c980e57d127a30466791d7c01b86e3
Thế Cường Lê <thecuong.work@gmail.com>
2024-06-02 05:15:27 +0700

    feat: change way get environ

--- chatbot.py
+++ chatbot.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-MODEL_NAME = os.getenv('MODEL_NAME')
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+MODEL_NAME = os.environ['MODEL_NAME']
 
 generation_config = {
   "temperature": 0.05,
